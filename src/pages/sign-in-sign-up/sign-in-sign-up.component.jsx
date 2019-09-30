360b9ed59be7e9fa7ed2d3b26944e5cb12cd9485
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-09-30 15:47:23 +0530

    Implimented all sign-up and signin function for the users authentication

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.compnent";
 import "./sign-in-sign-up.styles.scss";
 
 const SingInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
