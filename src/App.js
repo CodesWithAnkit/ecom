2c3606a3d9f4a161c3ed119b5378802c2172d617
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-10-03 10:21:16 +0530

    Including redux in the app

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ import ShopPage from "./pages/shop/shop.component";
 import SingInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.action";
 
 class App extends React.Component {
-  constructor() {
+  /* constructor() {
     super();
 
     this.state = {
       currentUser: null
     };
-  }
+  } */
+
+  /*we don't need this constructor anymore after applying redux, but why I didn't unserstand */
 
   unsubscribeFromAuth = null;
   // for signing the user
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-          // console.log(this.state);
         });
-      } else {
-        this.setState({ currentUser: userAuth });
       }
+      setCurrentUser(userAuth);
     });
   }
   // For signOut of the user
@@ -56,4 +56,11 @@ class App extends React.Component {
   }
 }
 
-export default connect(null)(App);
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
