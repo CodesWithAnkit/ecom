90f61259a03d25f32a0bf956fe6ab0458a9cd1b6
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-28 08:50:02 +0530

    Redux-persist for local-storage in the app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import "./index.css";
-
-import store from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <persistGate persistor={persistor}>
+        <App />
+      </persistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,23 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cart.reducer";
+import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 // import shoppingReducer from "./cart/shopping.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
   // shopping: shoppingReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
 
 // installing redux redux-logger react-redux for intiating redux
